93aa8026b0043ec47c0d7a54f8422a8c4ca0ce01
nhinojosa <nhinojosa@southtexascollege.edu>
2023-09-16 08:57:23 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+  import NavMenu from '@/components/NavMenu.vue'
+</script>
+
+<template>
+  <NavMenu />
+  <RouterView />
+</template>
---
+++ src/router.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import HomePage from '@/views/HomePage.vue'
+
+const routes = [
+  { path: '/', name: 'Home', component: HomePage },
+  { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
