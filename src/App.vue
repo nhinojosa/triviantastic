d8eaf4640dcb891aa333e84b55f9a3e2e18d1866
nhinojosa <nhinojosa@southtexascollege.edu>
2023-09-16 10:03:32 -0500

    Main Page Laylout

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,20 @@
 </script>
 
 <template>
-  <RouterView />
+  <main>
+    <div class="card">
+      <RouterView />      
+    </div>
+
+  </main>
+
 </template>
+
+<style lang="postcss" scoped>
+  main {
+    @apply flex justify-center items-center h-screen bg-gradient-to-r from-gray-700 via-gray-900 to-black;
+    & .card {
+      @apply flex h-5/6 min-h-fit w-11/12 flex-col items-center gap-16 overflow-auto rounded-lg bg-white shadow-lg;
+    }
+  }
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -4,5 +4,43 @@
 </script>
 
 <template>
-  <h1 class="text-6xl font-thin text-slate-800">I'm the home page</h1>
+  <div class="brand">
+    <img src="logo.svg" alt="" class="logo">
+    <h1 class="title">Triviantastic</h1>
+    <img src="logo.svg" alt="" class="logo">
+  </div>
+  <div class="categories">
+    <RouterLink :to="`/question/category/${n}`" v-for="n in 24" :key="n" class="category">
+      categories - {{ n }}
+    </RouterLink>
+  </div>
 </template>
+
+<style lang ="postcss" scoped>
+.brand {
+  @apply flex items-center justify-center gap-4;
+
+  & .logo {
+    @apply h-16 w-16;
+  }
+
+  & .title {
+    @apply text-6xl font-thin tracking-widest text-slate-700 uppercase;
+  }
+}
+
+.categories {
+  @apply grid flex-grow grid-cols-4 gap-12;
+
+  & .category {
+    @apply flex h-32 w-40 items-center justify-center rounded-lg border-4 border-slate-500 py-4 font-bold uppercase transition-colors duration-300;
+
+      &:hover {
+        @apply cursor-pointer border-red-500 bg-red-500 text-white;
+      }
+  }
+
+
+}
+
+</style>
